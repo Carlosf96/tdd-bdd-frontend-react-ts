9d254e6361826b505c8bafae235228622ef499f7
carlosf96 <carlos.f.roque96@gmail.com>
2019-11-12 09:44:49 -0600

    styled user list view

--- src/views/UserListView.tsx
+++ src/views/UserListView.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { User } from "../entities/User";
 import { IUserService } from "../services/UserService";
+import './userListView.css';
 
 export const UserListViewFactory = (userService: IUserService) => {
   const UserListView = () => {
@@ -38,38 +39,39 @@ export const UserListViewFactory = (userService: IUserService) => {
       );
     }
     return (
-      <div 
-      className="App"  
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-      }}>
-        <h1 
-        style={{
-          position: 'absolute',
-        }}>Welcome</h1>
-        <div 
-        style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          padding: '75px 0px 0px 0px',
-          justifyContent: 'space-evenly'
-        }}>
-          <Link to={"/add"}>ADD</Link>
-        
-        {Users.map(user => {
-          return (
-            <Link key={user._id} to={`/${user._id}`}>
-              <div>
-                {user.firstName + " " + user.lastName}
-                <Link key={user._id} to={`/${user._id}/edit`}>
-                  <p>Edit</p>
+      <div
+        className="container"
+      >
+        <h1 className='main-title'>
+          Welcome
+        </h1>
+        <div className='users-section'>
+          <Link to={"/add"} className='nice-button'>
+              ADD  
+          </Link>
+          {Users.map(user => {
+            return (
+              <div className='user-card'>
+                <Link 
+                key={user._id} 
+                to={`/${user._id}`}
+                style={{
+                  textDecoration: 'none',
+                }}
+                >
+                  {user.firstName + " " + user.lastName}
+                  <Link 
+                  key={user._id} 
+                  to={`/${user._id}/edit`}
+                  style={{
+                    textDecoration: 'none',
+                  }}>
+                    <p>Edit</p>
+                  </Link>
                 </Link>
               </div>
-            </Link>
-          );
-        })}
+            );
+          })}
         </div>
       </div>
     );
